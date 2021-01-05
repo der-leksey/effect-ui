303904ece8db5c298c3fd31d25b4d3bdeea49595
der-leksey <der.leksey@ya.ru>
2021-01-05 22:03:08 +0300

    0.2

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-import noUiSlider from 'nouislider';
-import 'nouislider/distribute/nouislider.css';
-window.noUiSlider = noUiSlider;
-
 import EFPlugin from './js/plugin.js';
 if (window.Vue) Vue.use(EFPlugin);
